118e939bb5adfd069178d01cd52e4aa37a75432f
Ahammed Fahim Galib <galib0124gmail.com>
2022-12-28 17:28:23 +0600

    post added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config()
 
@@ -9,9 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.w7wfspi.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 const run = async() => {
     try{
+        const alltask = client.db('task-collection').collection('allCollection');
+
+        app.post('/allTask', async(req, res) => {
+            const tasks = req.body;
+            const alltaskinsert = await alltask.insertOne(tasks);
+            res.send(alltaskinsert);
+        })
 
     }
     finally {
